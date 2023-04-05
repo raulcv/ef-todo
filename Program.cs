dea0a4b0305fefe775b69f51258e1a69aea82194
Raul <iraulcv@gmail.com>
2023-04-05 00:11:02 -0500

    EF todo operations

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using ef_todo;
+using ef_todo.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("EFTodoDB")); // Config using EF core to use Memory DB
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("EFTodoDB")); // Config using EF core to use Memory DB
+
 // builder.Services.AddSqlServer<TodoContext>("Data Source=PC-RAULCV-12343YU40\\SQLEXPRESS:Initial Catalog=EFTodoDB;user id=sa;password=mypassword");
 builder.Services.AddNpgsql<TodoContext>(
     builder.Configuration.GetConnectionString("EFTodoDBConnectionString")
@@ -20,6 +22,105 @@ app.MapGet(
     {
         dbcontext.Database.EnsureCreated();
         return Results.Ok("Data base in Mmemory: " + dbcontext.Database.IsInMemory());
+    }
+);
+
+app.MapGet(
+    "/api/tasks",
+    async ([FromServices] TodoContext dbContext) =>
+    {
+        return Results.Ok(
+            dbContext.Tasks.Include(p => p.Category).Where(p => p.Priority == Priority.Hight)
+        // .Where(p => p.DeletedAt != DateTime.Parse("1-1-1900").ToUniversalTime())
+        );
+    }
+);
+
+app.MapGet(
+    "/api/tasks/priority/{id}",
+    async ([FromServices] TodoContext dbContext, int id) =>
+    {
+        var data = dbContext.Tasks
+            .Include(a => a.Category)
+            .Where(
+                a =>
+                    (int)a.Priority == id
+                    && a.DeletedAt != DateTime.Parse("1-1-1900").ToUniversalTime()
+            );
+        return Results.Ok(data);
+    }
+);
+
+app.MapPost(
+    "/api/tasks",
+    async ([FromServices] TodoContext dbContext, [FromBody] TaskModel task) =>
+    {
+        task.Id = Guid.NewGuid();
+        task.CreatedAt = DateTime.UtcNow;
+        await dbContext.AddAsync(task);
+        // await dbContext.Tasks.AddAsync(task); Another way to add data to DB Using EF
+        await dbContext.SaveChangesAsync();
+        return Results.Ok("Task was deleted");
+    }
+);
+
+app.MapPut(
+    "/api/tasks/{id}",
+    async ([FromServices] TodoContext dbContext, [FromBody] TaskModel task, [FromRoute] Guid id) =>
+    {
+        var currentTask = dbContext.Tasks.Find(id);
+        if (currentTask != null)
+        {
+            currentTask.CategoryId = task.CategoryId;
+            currentTask.Title = task.Title;
+            currentTask.Description = task.Description;
+            currentTask.Priority = task.Priority;
+            currentTask.Done = task.Done;
+            currentTask.ModifiedAt = DateTime.UtcNow;
+            await dbContext.SaveChangesAsync();
+            return Results.Ok("Modified");
+        }
+        return Results.NotFound("Task not found");
+    }
+);
+app.MapPatch(
+    "/api/tasks/toggle/{id}",
+    async ([FromServices] TodoContext dbContext, [FromRoute] Guid id) =>
+    {
+        var currentTask = dbContext.Tasks.Find(id);
+        if (currentTask != null)
+        {
+            Console.WriteLine(currentTask.DeletedAt);
+            Console.WriteLine(DateTime.Parse("1-1-1900").ToUniversalTime());
+            if (currentTask.DeletedAt == DateTime.Parse("1-1-1900").ToUniversalTime())
+            {
+                currentTask.DeletedAt = DateTime.UtcNow;
+                await dbContext.SaveChangesAsync();
+                return Results.Ok("Tas was disabled");
+            }
+            else
+            {
+                currentTask.DeletedAt = DateTime.Parse("1-1-1900").ToUniversalTime();
+                await dbContext.SaveChangesAsync();
+                return Results.Ok("Tas was Enabled");
+            }
+        }
+        return Results.NotFound("Task not found");
+    }
+);
+
+app.MapDelete(
+    "/api/tasks/{id}",
+    async ([FromServices] TodoContext dbContext, [FromRoute] Guid id) =>
+    {
+        var currentTask = dbContext.Tasks.Find(id);
+        if (currentTask != null)
+        {
+            dbContext.Remove(currentTask);
+            await dbContext.SaveChangesAsync();
+            return Results.Ok("Tas was deleted");
+        }
+        return Results.NotFound("Task not found");
     }
 );
 
--- TodoContext.cs
+++ TodoContext.cs
@@ -1,13 +1,93 @@
 using Microsoft.EntityFrameworkCore;
-
+using ef_todo.Models;
 namespace ef_todo;
 
 public class TodoContext : DbContext
 {
     //Categories and Tasks represents all collections about themself
     public DbSet<Category> Categories { get; set; }
-    public DbSet<Task> Tasks { get; set; }
+    public DbSet<TaskModel> Tasks { get; set; }
 
     public TodoContext(DbContextOptions<TodoContext> options)
         : base(options) { }
+
+    // Re create = OVERRIDE always should be protected method no public
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        List<Category> categoriesInit = new List<Category>();
+        categoriesInit.Add(
+            new Category()
+            {
+                Id = Guid.Parse("fdbda192-37af-49e1-a125-21a5fa03e3e8"),
+                Name = "Pendant Activities",
+                Weight = 20
+            }
+        );
+        categoriesInit.Add(
+            new Category()
+            {
+                Id = Guid.Parse("fdbda192-37af-49e1-a125-21a5fa03e302"),
+                Name = "Personal Activities",
+                Weight = 50
+            }
+        );
+
+        modelBuilder.Entity<Category>(category =>
+        {
+            category.ToTable("Category"); //Category should be singular because database says that tables will be in singular
+            category.HasKey(p => p.Id);
+            category.Property(p => p.Name).IsRequired().HasMaxLength(150);
+            category.Property(p => p.Description).IsRequired(false);
+            category.HasData(categoriesInit); // Seed data
+        });
+
+        List<TaskModel> tasksInit = new List<TaskModel>();
+        tasksInit.Add(
+            new TaskModel()
+            {
+                Id = Guid.Parse("fdbda192-37af-49e1-a125-21a5fa03e3f4"),
+                CategoryId = Guid.Parse("fdbda192-37af-49e1-a125-21a5fa03e3e8"),
+                Title = "Pay Taxes in Lost country",
+                Description =
+                    "I need to go to the post office to get ticket and pay the ammount ehehe",
+                Priority = Priority.Medium,
+                Done = Status.False,
+                CreatedAt = DateTime.UtcNow
+            }
+        );
+        tasksInit.Add(
+            new TaskModel()
+            {
+                Id = Guid.Parse("fdbda192-37af-49e1-a125-21a5fa03e3f5"),
+                CategoryId = Guid.Parse("fdbda192-37af-49e1-a125-21a5fa03e302"),
+                Title = "Go to the Gym",
+                Description = "I need to 10 kilometers",
+                Priority = Priority.Hight,
+                Done = Status.False,
+                CreatedAt = DateTime.UtcNow
+            }
+        );
+
+        modelBuilder.Entity<TaskModel>(task =>
+        {
+            task.ToTable("Task");
+            task.HasKey(p => p.Id);
+            task.HasOne(p => p.Category).WithMany(p => p.Tasks).HasForeignKey(p => p.CategoryId);
+            task.Property(p => p.Title).IsRequired().HasMaxLength(200);
+            task.Property(p => p.Description).IsRequired(false);
+            task.Property(p => p.Priority);
+            // .HasConversion(v => v.ToString(), v => (priority)Enum.Parse(typeof(priority), v));
+            task.Property(p => p.CreatedAt).HasColumnName("Created_at");
+            task.Property(p => p.ModifiedAt)
+                .HasColumnName("Modified_at")
+                .HasDefaultValue(DateTime.Parse("01-01-1900").ToUniversalTime());
+            task.Property(p => p.DeletedAt)
+                .HasColumnName("Deleted_at")
+                .HasDefaultValue(DateTime.Parse("01-01-1900").ToUniversalTime());
+            task.Property(p => p.Done)
+                .HasConversion(v => v.ToString(), v => (Status)Enum.Parse(typeof(Status), v));
+            task.Ignore(p => p.Summary);
+            task.HasData(tasksInit);
+        });
+    }
 }
